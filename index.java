77ae99bf7b1bad03fa788fe1ec8effa322f32174
kokooldude <khalileoliver@gmail.com>
2020-11-10 18:24:49 +0000

    I'm really having trouble with the mouseX hover code.

--- index.java
+++ index.java
@@ -6,13 +6,58 @@ void draw() {
 	background(255);
 	stroke(0);
 
+
+
   //start creating the lines
+//box 1
+rect(5, 100, 80, 80);
+//box 2
+rect(100, 100, 80, 80);
+//box 3
+rect(195, 100, 80, 80);
+//box 4
+rect(290, 100, 80, 80);
 
+//S1 B1
+rect(5, 100, 40, 40);
+//S2 B1
+rect(45, 100, 40, 40);
+//S3 B1
+rect(5, 140, 40, 40);
 
+//S1 B2
+rect(100, 100, 40, 40);
+//S2 B2
+rect(140, 100, 40, 40);
+//S3 B2
+rect(100, 140, 40, 40);
+
+//S1 B3
+rect(195, 100, 40, 40);
+//S2 B3
+rect(235, 100, 40, 40);
+//S3 B3
+rect(195, 140, 40, 40);
+
+//S1 B4
+rect(290, 100, 40, 40);
+//S2 B4
+rect(330, 100, 40, 40);
+//S3 B4
+rect(290, 140, 40, 40);
   //Fill a black color
   
+  //rgb(0, 0, 0)
 
   //Build your conditional statement 
 	
+//if(MouseY = 100 && MouseX = 5){
+  //fill(0, 0, 0);
+//rect(5, 100, 40, 40);
+//}
+  
+
+
+
 
 }
